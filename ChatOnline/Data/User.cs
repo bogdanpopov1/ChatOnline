f029221a9dc8a924bdae831e715d9fe95f8ab3d0
Богдан Попов <bogdan_popov17@mail.ru>
2024-04-21 15:18:41 +0300

    Регистрация и вход перенесены в модальные окна, оформлена шапка (окна не открываются)

---
+++ ChatOnline/Data/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatOnline.Data
+{
+    public class User
+    {
+        public User(string username, string password)
+        {
+            Id = Guid.NewGuid().ToString();
+            Username = username;
+            Password = password;
+        }
+
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ChatOnline/Data/db_connection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ChatOnline.Data
+{
+    public class db_connection : ComponentBase
+    {
+        [Parameter]
+        public List<User> usersList { get; set; } = new List<User>();
+
+        [Parameter]
+        public bool registrationTrigger { get; set; } = false;
+
+        [Parameter]
+        public bool signInTrigger { get; set; } = false;
+
+    }
+}
